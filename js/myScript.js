237119783461517551a34a6885d50cb80f65fac3
zhouzzhe <zzhe828@gmail.com>
2024-08-06 17:33:45 +0800

    好康報報

--- js/myScript.js
+++ js/myScript.js
@@ -15,3 +15,13 @@ function openLink(evt, linkName) {
 
 // Click on the first tablink on load
 document.getElementsByClassName("tablink")[0].click();
+var modalElem = document.getElementById("tyModal");
+var spanElem = document.getElementsByClassName("tyClose")[0];
+
+spanElem.onclick = function () {
+    modalElem.style.display = "none";
+};
+
+setTimeout(function () {
+    modalElem.style.display = "block";
+}, 1000);
