556a8d88cf2c23a521819da3d1321b81a93dd712
zhouzzhe <zzhe828@gmail.com>
2024-08-06 18:00:59 +0800

    Revert "w3school範本"
    This reverts commit c2de2ee812b84b419dd92569ac895e0b45aab8a1.

---
+++ js/myScript.js
@@ -0,0 +1,27 @@
+// Tabs
+function openLink(evt, linkName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("myLink");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(linkName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
+}
+
+// Click on the first tablink on load
+document.getElementsByClassName("tablink")[0].click();
+var modalElem = document.getElementById("tyModal");
+var spanElem = document.getElementsByClassName("tyClose")[0];
+
+spanElem.onclick = function () {
+    modalElem.style.display = "none";
+};
+
+setTimeout(function () {
+    modalElem.style.display = "block";
+}, 1000);
